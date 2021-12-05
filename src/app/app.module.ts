bfc3374488ec397e175a8bc1f45a25ff4a3dd2c8
Jim Fulto <jimfulto233@gmail.com>
2021-12-05 14:56:32 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeItemComponent } from './employees/employee-list/employee-item/employee-item.component';
+import { EmployeeDetailComponent } from './employees/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './employees/employee-edit/employee-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    HeaderComponent,
+    EmployeeListComponent,
+    EmployeeItemComponent,
+    EmployeeDetailComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
